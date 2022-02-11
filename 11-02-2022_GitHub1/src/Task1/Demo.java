c8700fa00bf43787afe0fa7c5b2881728cda6146
Dell <Dell@shubham>
2022-02-11 15:27:29 +0530

    by shubham

---
+++ 11-02-2022_GitHub1/src/Task1/Demo.java
@@ -0,0 +1,24 @@
+package Task1;
+
+public class Demo {
+	
+	public void m1()
+	{
+		System.out.println("M1 method");
+		
+	}
+
+	public void m2()
+	{
+		System.out.println("M2 method");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Demo d=new Demo();
+		d.m1();
+		d.m2();
+		
+	}
+}
